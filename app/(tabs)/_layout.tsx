b9a2b37c53eca2c1c3f25ba2c2fa28c07c07dac4
desmond tan <desmond@desmonds-MacBook-Pro.local>
2026-03-13 14:32:00 +0800

    Created skeleton

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,30 @@
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+    return (
+        <Tabs>
+            <Tabs.Screen
+                name="index"
+                options={{
+                    headerTitle: "Home",
+                    title: "Home",
+                }
+                }
+            />
+            <Tabs.Screen
+                name="new-note"
+                options={{
+                    headerTitle: "New Note",
+                    title: "New Note",
+                }
+                }
+            />
+            <Tabs.Screen
+                name="summary"
+                options={{
+                    title: "Summary",
+                }}
+            />
+        </Tabs>
+    );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(tabs)" />
+      <Stack.Screen name="new-note" />
+      <Stack.Screen name="settings" />
+    </Stack>
+  );
 }
